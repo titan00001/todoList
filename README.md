ee91623d03739ee504c86b70b4e39ddbaff9e214
titan00001 <gplay9792@gmail.com>
2019-11-30 00:14:22 +0530

    features added: remove and add items

---
+++ README.md
@@ -0,0 +1,7 @@
+this is branch of todo list
+
+NO banckend technology is used
+
+caching or local storge of js is not used
+
+no custom styling is used
--- addItem.js
+++ addItem.js
@@ -1,14 +1,74 @@
-list = ['Samosa', 'chuney', 'rice'];
+var list = ['Samosa', 'chuney', 'rice'];
 
-function displayItem(list){
+
+// remove redundancy of declaring container from two function
+function displayItem(){
     for (var l in list){
 
         var li = document.createElement("p");
-        li.innerHTML = list[l];
+        var btn = document.createElement("button");
+        li.textContent = list[l];
+
         document.querySelector(".item-display").appendChild(li);
+        li.appendChild(btn);
+        btn.setAttribute('class','remove-item-btn');
+        btn.textContent = "Remove";
+
+        btn.addEventListener('click',  function(e) {
+            removeItem(e);
+        });
     }
 }
 
-function addItem()
+function removeItem(e) {
+    
+    // removes the parent container of button clicked
+    var item = e.target;
+    var text = item.parentNode.firstChild.textContent;
+    item.parentNode.parentNode.removeChild(item.parentNode);
 
-displayItem(list);
+    removeFromList(text);
+
+}
+
+function removeFromList(item) {
+    
+    var newlist = [];
+    for (var i = 0; i < list.length; i++) {
+        if (list[i] !== item) {
+            newlist.push(list[i]);
+        }
+    }
+
+    list = newlist;
+
+}
+
+
+function addItem(){
+
+    
+    var item = document.querySelector(".item-input").value;
+
+    if(item != ""){
+
+        list.push(item);
+
+        var li = document.createElement("p");
+        var btn = document.createElement("button");
+        li.textContent = item;
+        document.querySelector(".item-display").appendChild(li);
+
+        li.appendChild(btn);
+        btn.setAttribute('class','remove-item-btn');
+        btn.textContent = "Remove";
+
+        btn.addEventListener('click',  function(e) {
+            removeItem(e);
+        });
+    }
+
+
+}
+
+displayItem();
