4345c4fa7a7a028ad10062e9ddb7c0dbc6d5fb6e
titan00001 <gplay9792@gmail.com>
2019-11-29 13:51:30 +0530

    new layout, only display iem from array

--- addItem.js
+++ addItem.js
@@ -1,114 +1,14 @@
-// pendingList = [
-//     {
-//         "product" : ["tomato", "potato", "sauce"],
-//         "date-created" : "23/10/19",
-//         "favourite" : true
+list = ['Samosa', 'chuney', 'rice'];
 
-//     },
+function displayItem(list){
+    for (var l in list){
 
-//     {
-//         "product" : ["surf", "detergent", "bleaching powder"],
-//         "date-created" : "02/01/19",
-//         "favourite" : true
-
-//     },
-
-//     {
-//         "product" : ["bulb", "fan", "tubelight"],
-//         "date-created" : "23/08/19",
-//         "favourite" : true
-
-//     },
-
-//     {
-//         "product" : ["lays", "samosa", "dhokla"],
-//         "date-created" : "02/10/19",
-//         "favourite" : true
-
-//     },
-
-// ];
-
-currentProductList = ["bulb", "fan", "tubelight"];
-addEventToItem();
-
-function addItem() {
-
-    let input = document.querySelector("#inputItem");
-    let item = input.value;
-
-    if(input.value == "")
-        console.log("enter valid item");
-    else {      
-        currentProductList.push(item);
-        console.log("item added");
-        }
-
-    input.value = "";
-
+        var li = document.createElement("p");
+        li.innerHTML = list[l];
+        document.querySelector(".item-display").appendChild(li);
+    }
 }
 
+function addItem()
 
-
-function updateItemList() {
-    
-    currentProductList.forEach(item => {
-        
-        console.log(item);
-    });
-
-}
-
-function addEventToItem() {
-    
-    let itemsBtn = document.querySelectorAll(".itemRemove");
-
-    // add event to item : remove
-    itemsBtn.forEach(item => {
-
-        item.addEventListener("click", function(e){
-
-            console.log(item.textContent);
-            // Add remove event
-            removeItem(item.textContent);
-
-
-        });
-
-        let items
-
-        item.addEventListener("dblclick", function(e){
-
-            console.log(item.textContent + " double clicked");
-            // Add remove event
-            editItem(item.textContent);
-
-
-        });
-
-    });
-
-}
-
-function removeItem(itemName){
-
-    if(currentProductList.includes(itemName))
-        console.log("item found and removed");
-    else
-        console.log("some data have not been stored")
-
-}
-
-function editItem(text){
-
-    document.querySelector("#inputItem").value = text;
-}
-
-function saveItemArrayToDB(){
-    console.log("all data saved to db");
-}
-
-function clearAll() {
-    currentProductList = [];
-    console.log("all reset");
-}
+displayItem(list);
